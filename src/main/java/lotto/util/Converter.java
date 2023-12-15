a737528fa6fc4a11962f01664e48ce06dc8a6abf
taehoon <xogns1514@gmail.com>
2023-12-15 17:05:46 +0900

    feat(InputView): 당첨 번호 입력받기

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -7,6 +7,12 @@ public class Converter {
 
     private static final String COMMA = ",";
 
+    public static List<Integer> convertToIntegerList(String input) {
+        return Stream.of(input.split(COMMA))
+                .map(Integer::parseInt)
+                .toList();
+    }
+
     public static List<String> splitWithComma(String input) {
         return Stream.of(input.split(COMMA))
                 .toList();
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,15 +3,28 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import lotto.exception.ErrorMessage;
+import lotto.util.Converter;
 import lotto.valid.InputValidator;
+import lotto.valid.NumberValidator;
 import lotto.valid.PurchaseMoneyValidator;
 
 public class InputView {
 
+    private static final String INPUT_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
+
     public static int inputPurchaseMoney() {
+        System.out.println(INPUT_PURCHASE_MONEY);
         String purchaseMoney = Console.readLine();
         PurchaseMoneyValidator.validate(purchaseMoney);
         return Integer.parseInt(purchaseMoney);
+    }
+
+    public static List<Integer> inputWinningLotto() {
+        System.out.println(INPUT_WINNING_LOTTO);
+        String winningNumbers = Console.readLine();
+        NumberValidator.validate(winningNumbers);
+        return Converter.convertToIntegerList(winningNumbers);
     }
 
 }
