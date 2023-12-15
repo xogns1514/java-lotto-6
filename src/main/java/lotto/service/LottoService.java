01a14b66c7142661deea2cc2bd06675802c62974
taehoon <xogns1514@gmail.com>
2023-12-15 19:19:42 +0900

    feat(LottoController): 로또 실행

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lotto.domain.Lotto;
@@ -10,12 +11,24 @@ import lotto.repository.MatchRepository;
 
 public class LottoService {
 
-    private final MatchRepository matchRepository = new MatchRepository();
+    private final MatchRepository matchRepository;
     private static final int RATIO = 100;
+
+    public LottoService() {
+        matchRepository = new MatchRepository();
+    }
 
     public double getProfitRatio(int purchaseMoney) {
         int totalPrize = getTotalPrize();
         return ((double) totalPrize / purchaseMoney) * RATIO;
+    }
+
+    public List<Integer> getInformation(Rank rank) {
+        List<Integer> info = new ArrayList<>();
+        info.add(rank.getMatchNumber());
+        info.add(rank.getPrize());
+        info.add(matchRepository.getMatchRankCount(rank));
+        return info;
     }
 
     private int getTotalPrize() {
